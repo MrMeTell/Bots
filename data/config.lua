66259144ae26aeea2c71954a9a45cc1affccba1d
MR.me <36264428+MrMeTell@users.noreply.github.com>
2018-02-13 12:19:59 +0000

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,17 @@
+do local _ = {
+	plug = {
+    "TD"
+  },
+  Helper = "darkplusBot",
+  Token = "542247211:AAHaMvqyVl3BPkdZ7riJgs7xwYZVZB50kgs",
+  bot_owner = {
+    360661957,
+    532704716
+  },
+  cmd = "^[/!#]",
+  sudo_users = {
+    437978064
+  }
+}
+return _
+end
